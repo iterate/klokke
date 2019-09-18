552f771d1dd476fbb8022c24da35c5cc6783d98a
Oddmund Strømme <oddmunds@iterate.no>
2019-09-18 13:45:45 +0200

    more

--- src/Face.elm
+++ src/Face.elm
@@ -5,35 +5,81 @@ import Html
 import Segments exposing (segment0, segment1, segment2, segment3, segment4, segment5, segment6)
 import Time exposing (Posix)
 import TypedSvg
-import TypedSvg.Attributes exposing (fill, viewBox)
+import TypedSvg.Attributes exposing (fill, transform, viewBox)
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (Fill(..))
+import TypedSvg.Types exposing (Fill(..), Transform(..))
 
 
 view : Posix -> Html.Html msg
 view posix =
     let
-        digit =
+        seconds =
             Time.toSecond Time.utc posix
                 |> String.fromInt
-                |> String.right 1
+                |> String.padLeft 2 '0'
+
+        second0 =
+            seconds |> String.right 1
+
+        second1 =
+            seconds
+                |> String.right 2
+                |> String.left 1
+
+        minutes =
+            Time.toMinute Time.utc posix
+                |> String.fromInt
+                |> String.padLeft 2 '0'
+
+        minute0 =
+            minutes |> String.right 1
+
+        minute1 =
+            minutes
+                |> String.right 2
+                |> String.left 1
+
+        hours =
+            Time.toHour Time.utc posix
+                |> String.fromInt
+                |> String.padLeft 2 '0'
+
+        hour0 =
+            hours |> String.right 1
+
+        hour1 =
+            hours
+                |> String.right 2
+                |> String.left 1
     in
-    TypedSvg.svg [ viewBox 0 0 1000 1600 ]
-        [ segm digit [ "1", "4" ] segment0
-        , segm digit [ "1", "2", "3", "7" ] segment1
-        , segm digit [ "5", "6" ] segment2
-        , segm digit [ "0", "1", "7" ] segment3
-        , segm digit [ "1", "3", "4", "5", "7", "9" ] segment4
-        , segm digit [ "2" ] segment5
-        , segm digit [ "1", "4", "7", "9" ] segment6
+    TypedSvg.svg [ viewBox 0 0 1000 160 ]
+        [ digit hour1 0
+        , digit hour0 100
+        , digit minute1 200
+        , digit minute0 300
+        , digit second1 400
+        , digit second0 500
+        ]
+
+
+digit : String -> Float -> Svg msg
+digit second offset =
+    TypedSvg.g [ transform [ Translate offset 0 ] ]
+        [ segm second [ "1", "4" ] segment0
+        , segm second [ "1", "2", "3", "7" ] segment1
+        , segm second [ "5", "6" ] segment2
+        , segm second [ "0", "1", "7" ] segment3
+        , segm second [ "1", "3", "4", "5", "7", "9" ] segment4
+        , segm second [ "2" ] segment5
+        , segm second [ "1", "4", "7", "9" ] segment6
         ]
 
 
 segm : String -> List String -> Svg msg -> Svg msg
-segm digit filter path =
+segm second filter path =
     let
         status =
-            not <| List.member digit filter
+            not <| List.member second filter
     in
     TypedSvg.g [ fill <| Fill (kulour status) ] [ path ]
 
