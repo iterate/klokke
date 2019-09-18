2bd52e215e74b96bc4005b1a10203f937e33b2f7
Oddmund Strømme <oddmunds@iterate.no>
2019-09-18 13:48:02 +0200

    Remove text watch

--- src/Main.elm
+++ src/Main.elm
@@ -39,15 +39,8 @@ view : Model -> Html msg
 view model =
     Element.layout [ Element.Background.color (Element.rgb255 0xBC 0xC1 0xC1) ]
         (Element.column []
-            [ Element.text (textWatch model)
-            , Element.html (Face.view model.time)
-            ]
+            [ Element.html (Face.view model.time) ]
         )
-
-
-textWatch : Model -> String
-textWatch model =
-    paddedHours model ++ ":" ++ paddedMinutes model ++ ":" ++ paddedSeconds model
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
@@ -60,45 +53,3 @@ update msg _ =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Time.every 1000 Tick
-
-
-paddedSeconds : Model -> String
-paddedSeconds model =
-    let
-        unpadded =
-            String.fromInt (Time.toSecond model.zone model.time)
-    in
-    case String.length unpadded of
-        1 ->
-            "0" ++ unpadded
-
-        _ ->
-            unpadded
-
-
-paddedMinutes : Model -> String
-paddedMinutes model =
-    let
-        unpadded =
-            String.fromInt (Time.toMinute model.zone model.time)
-    in
-    case String.length unpadded of
-        1 ->
-            "0" ++ unpadded
-
-        _ ->
-            unpadded
-
-
-paddedHours : Model -> String
-paddedHours model =
-    let
-        unpadded =
-            String.fromInt (Time.toHour model.zone model.time)
-    in
-    case String.length unpadded of
-        1 ->
-            "0" ++ unpadded
-
-        _ ->
-            unpadded
